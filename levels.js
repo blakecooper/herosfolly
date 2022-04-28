93003b174348a9278f2e6183ba1065390547557a
Blake Cooper <contact@blakecooper.us>
2022-04-28 15:58:23 -0400

    Random level generation!
    Algo now allows for a small (10%) chance of rooms not appearing. When this happens, the hallways still manage to connect to other rooms WITH HIGH PROBABILITY (not that it's not 100%; there's still a SMALL chance that an important room won't connect, but after testing I can confidently say it's no more than 3%. Will try to improve with future iterations.

--- levels.js
+++ levels.js
@@ -3,7 +3,6 @@ const MIN_ROOM_SIZE = 5;
 const MAX_ROOM_SIZE = 8;
 const ROWS = 16;
 const COLS = 32;
-const NUMBER_LEVELS_TO_GENERATE = 30;
 
 function stringToHash(string) {
                   
@@ -20,121 +19,171 @@ function stringToHash(string) {
                 return hash;
             }
 
+function generateHallways(level, roomStats, roomRows, roomCols) {
+    for (let roomRow = 0; roomRow < roomRows; roomRow++) {
+        for (let roomCol = 0; roomCol < roomCols; roomCol++) {
+            
+            let statsIdx = (roomRow * roomCols) + roomCol;
+
+            if (roomStats[statsIdx][0] !== null && roomCol < roomCols - 1) {
+                let x = Math.floor((roomStats[statsIdx][0] + roomStats[statsIdx][2]) / 2) + (roomRow * MAX_ROOM_SIZE);
+                let y = Math.floor((roomStats[statsIdx][1] + roomStats[statsIdx][3]) / 2) + (roomCol * MAX_ROOM_SIZE);
+
+                //Move from the middle of the room to the nearest wall
+                let ySteps = 0;
+
+                while (level[x][y + ySteps] !== WALL) {
+                    ySteps++;
+                }
+
+                y += ySteps;
+
+                level[x][y] = FLOOR;
+
+                y++;
+
+                //Edge case: room is one room away from the right, and there's no room on the right
+                let roomOnTheRight = true;
+
+                if (statsIdx === ((roomRow * roomCols) + roomCols - 1)) {
+                    if (roomStats[(roomRow * roomCols) + roomCols - 1] === null) {
+                        roomOnTheRight = false;
+                    }
+                }
+            
+                while(y < COLS && level[x][y] !== FLOOR) {
+                    level[x][y] = FLOOR;
+
+                    if (x-1 > 0) {
+                        level[x-1][y] = WALL;
+                    }
+
+                    if (x+1 < ROWS) {
+                        level[x+1][y] = WALL;
+                    }
+                
+                    y++;
+                }
+
+                //Edge case: a hallway leads off the edge of the map (cap it with a wall)
+                if (y === (COLS)){
+                    level[x][y-1] = WALL;
+                }
+            }
+
+
+            if (roomStats[statsIdx][0] !== null && roomRow < roomRows - 1) {
+            
+                //Same but for down
+                x = Math.floor((roomStats[statsIdx][0] + roomStats[statsIdx][2]) / 2) + (roomRow * MAX_ROOM_SIZE);
+                y = Math.floor((roomStats[statsIdx][1] + roomStats[statsIdx][3]) / 2) + (roomCol * MAX_ROOM_SIZE);
+
+                //Find the wall
+                if (level[x][y] !== WALL) {
+                    while (level[x][y] !== WALL && x < ROWS) {
+                        x++;
+                    }
+                }
+            
+                level[x][y] = FLOOR;
+
+                x++;
+
+                while (x < ROWS && level[x][y] !== FLOOR) {
+                    level[x][y] = FLOOR;
+
+                    if (y-1 > 0) {
+                        level[x][y-1] = WALL;
+                    }
+
+                    if (y+1 < COLS) {
+                        level[x][y+1] = WALL;
+                    }
+    
+                    x++;
+                }
+
+                //Edge case: hallways leads off the bottom of the map (cap it with a wall)
+                if (x === (ROWS)){
+                    level[x-1][y] = WALL;
+                }
+            }
+        }
+    }
+
+    return level;
+}
+
 function generateLevel(string = "default") {
 
     let level = initializeMatrix(ROWS, COLS, null);
 
     let roomStats = [];
-    //divide level into 4 x 2
-    //generate random room size from 5 to 8
-    //small chance of room not forming
-    //otherwise: print room to square
-    //if smaller than 8, offset slightly
  
     let roomRows = Math.floor(ROWS/MAX_ROOM_SIZE);
     let roomCols = Math.floor(COLS/MAX_ROOM_SIZE);
    
-    //TODO: introduce (a stable version of) rooms not showing up w/ small probability
     for (let roomRow = 0; roomRow < roomRows; roomRow++) {
         for (let roomCol = 0; roomCol < roomCols; roomCol++) {
-            let room = generateRoom();
 
-            let rowOffset = random(MAX_ROOM_SIZE - room.length);
-            let colOffset = random(MAX_ROOM_SIZE - room[0].length);
+            let room = null;
+            roomStats.push([null,null,null,null]);
+            let rowOffset = null;
+            let colOffset = null;
 
-            let data = [room.length, room[0].length, rowOffset, colOffset];
+            //Random chance of room not appearing
+            if (random(10) > 1){
+                room = generateRoom();
 
-            roomStats.push(data);
+                rowOffset = random(MAX_ROOM_SIZE - room.length);
+                colOffset = random(MAX_ROOM_SIZE - room[0].length);
 
-            for (let row = 0; row < room.length; row++) {
-                for (let col = 0; col < room[0].length; col++) {
-                    level[(MAX_ROOM_SIZE * roomRow) + rowOffset + row][(MAX_ROOM_SIZE * roomCol) + colOffset + col] = room[row][col];
+                let data = [room.length, room[0].length, rowOffset, colOffset];
+
+                roomStats[(roomRow * roomCols) + roomCol] = data;
+                
+            }
+
+            if (room !== null) {
+                for (let row = 0; row < room.length; row++) {
+                    for (let col = 0; col < room[0].length; col++) {
+                        level[(MAX_ROOM_SIZE * roomRow) + rowOffset + row][(MAX_ROOM_SIZE * roomCol) + colOffset + col] = room[row][col];
+
+                    }
                 }
             }
         }
     }
 
 
-    //TODO: account for highly unlikely chance (one in a hundred million) that no rooms generate
+    //Account for unlikely chance that no rooms generate
+    let noRooms = true;
 
-    for (let roomRow = 0; roomRow < roomRows; roomRow++) {
-        for (let roomCol = 0; roomCol < roomCols; roomCol++) {
-
-            if (roomCol < roomCols - 1) {
-            let x = Math.floor((roomStats[(roomRow * roomCols) + roomCol][0] + roomStats[(roomRow * roomCols) + roomCol][2]) / 2) + (roomRow * MAX_ROOM_SIZE);
-            let y = Math.floor((roomStats[(roomRow * roomCols) + roomCol][1] + roomStats[(roomRow * roomCols) + roomCol][3]) / 2) + (roomCol * MAX_ROOM_SIZE);
-
-            let ySteps = 0;
-
-            while (level[x][y + ySteps] !== WALL) {
-                ySteps++;
-            }
-
-            y += ySteps;
-
-            level[x][y] = FLOOR;
-
-            y++;
-
-            while(y < COLS && level[x][y] !== FLOOR) {
-                level[x][y] = FLOOR;
-
-                if (x-1 > 0) {
-                level[x-1][y] = WALL;
-                }
-
-                if (x+1 < ROWS) {
-                level[x+1][y] = WALL;
-                }
-                y++;
-            }
-            }
-
-
-            if (roomRow < roomRows - 1) {
-            //Same but for down
-            x = Math.floor((roomStats[(roomRow * roomCols) + roomCol][0] + roomStats[(roomRow * roomCols) + roomCol][2]) / 2) + (roomRow * MAX_ROOM_SIZE);
-            y = Math.floor((roomStats[(roomRow * roomCols) + roomCol][1] + roomStats[(roomRow * roomCols) + roomCol][3]) / 2) + (roomCol * MAX_ROOM_SIZE);
-
-            //Find the wall
-            if (level[x][y] !== WALL) {
-                while (level[x][y] !== WALL && x < ROWS) {
-                    x++;
-                }
-            }
-            level[x][y] = FLOOR;
-
-            x++;
-
-            while (x < ROWS && level[x][y] !== FLOOR) {
-                level[x][y] = FLOOR;
-
-                if (y-1 > 0) {
-                    level[x][y-1] = WALL;
-                }
-
-                if (y+1 < COLS) {
-                    level[x][y+1] = WALL;
-                }
-                x++;
-            }
-
-            if (x === (ROWS - 1)){
-                level[x][y] = WALL;
-            }
-
-            }
+    for (let i = 0; i < roomStats.length; i++) {
+        if (roomStats[i][0] !== null) {
+            noRooms = false;
         }
     }
 
-    //connect with hallways:
-    //find middle right wall
-    //find middle down wall
-    //break right wall, replace with floor
-    //move one right, if not wall:
-    //  place floor, place wall above and below
-    //if wall: make floor 
-    //repeat for down
+    if (noRooms) {
+        let room = generateRoom();
+                
+        rowOffset = random(MAX_ROOM_SIZE - room.length);
+        colOffset = random(MAX_ROOM_SIZE - room[0].length);
+
+        roomStats.push([room.length, room[0].length, rowOffset, colOffset]);
+
+        for (let row = 0; row < room.length; row++) {
+            for (let col = 0; col < room[0].length; col++) {
+                level[(MAX_ROOM_SIZE * roomRow) + rowOffset + row][(MAX_ROOM_SIZE * roomCol) + colOffset + col] = room[row][col];
+
+            }
+        }
+
+    }
+   
+    level = generateHallways(level, roomStats, roomRows, roomCols);
+    
     return level;
 }
 
