09dbd80b0452403a0d56af21c2ec947befe84438
Blake Cooper <contact@blakecooper.us>
2022-08-02 13:26:13 -0400

    Refactoring levels.js (in progress)

--- levels.js
+++ levels.js
@@ -1,12 +1,6 @@
 let LEVEL = {
 
-  setSeed: function (str) {
-
-  },
-
-  seed: "",
-  
-  generateHallways: function (level, roomStats, roomRows, roomCols) {
+  generateHallways: function (map, roomStats, roomRows, roomCols) {
     for (let roomRow = 0; roomRow < roomRows; roomRow++) {
       for (let roomCol = 0; roomCol < roomCols; roomCol++) {
             
@@ -14,8 +8,9 @@ let LEVEL = {
 
         if (roomStats[statsIdx][0] !== null && roomCol < roomCols - 1) {
           let x = 
-          Math.floor((roomStats[statsIdx][0] + roomStats[statsIdx][2]) / 2) 
-          + (roomRow * RAWS.settings.max_room_size);
+          Math.floor(
+            (roomStats[statsIdx][0] + roomStats[statsIdx][2]) / 2
+          ) + (roomRow * RAWS.settings.max_room_size);
                 
           let y = 
           Math.floor((roomStats[statsIdx][1] + roomStats[statsIdx][3]) / 2) 
@@ -24,13 +19,13 @@ let LEVEL = {
           //Move from the middle of the room to the nearest wall
           let ySteps = 0;
 
-          while (level[x][y + ySteps] !== RAWS.map.text.wall) {
+          while (map[x][y + ySteps] !== RAWS.map.text.wall) {
             ySteps++;
           }
 
           y += ySteps;
 
-          level[x][y] = RAWS.map.text.floor;
+          map[x][y] = RAWS.map.text.floor;
 
           y++;
 
@@ -44,15 +39,15 @@ let LEVEL = {
             }
           }
             
-          while(y < RAWS.settings.cols && level[x][y] !== RAWS.map.text.floor) {
-            level[x][y] = RAWS.map.text.floor;
+          while(y < RAWS.settings.cols && map[x][y] !== RAWS.map.text.floor) {
+            map[x][y] = RAWS.map.text.floor;
 
             if (x-1 > 0) {
-              level[x-1][y] = RAWS.map.text.wall;
+              map[x-1][y] = RAWS.map.text.wall;
             }
 
             if (x+1 < RAWS.settings.rows) {
-              level[x+1][y] = RAWS.map.text.wall;
+              map[x+1][y] = RAWS.map.text.wall;
             }
                 
             y++;
@@ -60,7 +55,7 @@ let LEVEL = {
 
           //Edge case: a hallway leads off the edge of the map (cap it with a wall)
           if (y === (RAWS.settings.cols)){
-            level[x][y-1] = RAWS.map.text.wall;
+            map[x][y-1] = RAWS.map.text.wall;
           }
         }
 
@@ -75,25 +70,25 @@ let LEVEL = {
           + (roomCol * RAWS.settings.max_room_size);
 
           //Find the wall
-          if (level[x][y] !== RAWS.map.text.wall) {
-            while (level[x][y] !== RAWS.map.text.wall && x < RAWS.settings.rows) {
+          if (map[x][y] !== RAWS.map.text.wall) {
+            while (map[x][y] !== RAWS.map.text.wall && x < RAWS.settings.rows) {
               x++;
             }
           }
             
-          level[x][y] = RAWS.map.text.floor;
+          map[x][y] = RAWS.map.text.floor;
 
           x++;
 
-          while (x < RAWS.settings.rows && level[x][y] !== RAWS.map.text.floor) {
-            level[x][y] = RAWS.map.text.floor;
+          while (x < RAWS.settings.rows && map[x][y] !== RAWS.map.text.floor) {
+            map[x][y] = RAWS.map.text.floor;
 
             if (y-1 > 0) {
-              level[x][y-1] = RAWS.map.text.wall;
+              map[x][y-1] = RAWS.map.text.wall;
             }
 
             if (y+1 < RAWS.settings.cols) {
-              level[x][y+1] = RAWS.map.text.wall;
+              map[x][y+1] = RAWS.map.text.wall;
             }
     
             x++;
@@ -101,53 +96,64 @@ let LEVEL = {
 
           //Edge case: hallways leads off the bottom of the map (cap it with a wall)
           if (x === (RAWS.settings.rows)){
-            level[x-1][y] = RAWS.map.text.wall;
+            map[x-1][y] = RAWS.map.text.wall;
           }
         }
       }
     }
-    return level;
+    return map;
   },
 
-  generate: function (seedStr = "default") {
+  roomStats: [],
 
-    let level = initializeMatrix(RAWS.settings.rows, RAWS.settings.cols, null);
+  generate: function () {
 
-    this.seed = this.setSeed(seedStr);
+    let map = initializeMatrix(
+      RAWS.settings.rows, 
+      RAWS.settings.cols, 
+      null
+    );
 
-    let allConnected = false;
+    let connected = false;
 
-    while (!allConnected) {
+    while (!connected) {
 
-      let roomStats = [];
-
-      let roomRows = Math.floor(RAWS.settings.rows/RAWS.settings.max_room_size);
-      let roomCols = Math.floor(RAWS.settings.cols/RAWS.settings.max_room_size);
-   
+      let roomRows =                                                 //partition map into zones
+        Math.floor(RAWS.settings.rows/RAWS.settings.max_room_size);  //where rooms can generate
+      let roomCols = 
+        Math.floor(RAWS.settings.cols/RAWS.settings.max_room_size);
+  
       for (let roomRow = 0; roomRow < roomRows; roomRow++) {
         for (let roomCol = 0; roomCol < roomCols; roomCol++) {
 
-          let room = null;
-          roomStats.push([null,null,null,null]);
-          let rowOffset = null;
-          let colOffset = null;
-
+          //TODO: put this back and see what happens!
+            //NOTE: will need to declare null stats in an else statement!
           //Random chance of room not appearing
           //    if (random(10)) > 1){
-            room = this.generateRoom();
+            
+          let room = this.generateRoom();
 
-            rowOffset = random(RAWS.settings.max_room_size - room.length);
-            colOffset = random(RAWS.settings.max_room_size - room[0].length);
+          let rowOffset = random(                                    //determines how far from
+            RAWS.settings.max_room_size - room.length                //the edges of the zone
+          );                                                         //the room is placed
+          
+          let colOffset = random(
+            RAWS.settings.max_room_size - room[0].length
+          );
 
-            let data = [room.length, room[0].length, rowOffset, colOffset];
+          this.roomStats[(roomRow * roomCols) + roomCol].push({      //store room gen data
+            width: room.length,                                      //for connecting hallways
+            height: room[0].length,
+            rowOffset: rowOffset,
+            colOffset: colOffset
+          });
 
-            roomStats[(roomRow * roomCols) + roomCol] = data;
-            //   } else { console.log("Room not generated"); }
-
+          //   } else { console.log("Room not generated"); }
+//KEEP REFACTORING STARTING HERE!
           if (room !== null) {
             for (let row = 0; row < room.length; row++) {
               for (let col = 0; col < room[0].length; col++) {
-                level
+                map
                 [(RAWS.settings.max_room_size * roomRow) + rowOffset + row]
                 [(RAWS.settings.max_room_size * roomCol) + colOffset + col] 
                 = room[row][col];
@@ -179,7 +185,7 @@ let LEVEL = {
 
         for (let row = 0; row < room.length; row++) {
           for (let col = 0; col < room[0].length; col++) {
-            level
+            map
             [(RAWS.settings.max_room_size * roomRow) + rowOffset + row]
             [(RAWS.settings.max_room_size * roomCol) + colOffset + col] 
             = room[row][col];
@@ -187,35 +193,35 @@ let LEVEL = {
         }
       }
    
-      level = this.generateHallways(level, roomStats, roomRows, roomCols);
-      level = this.generateBorder(level);
+      map = this.generateHallways(map, roomStats, roomRows, roomCols);
+      map = this.generateBorder(map);
 
-      if (this.isConnected(level)) {
-        allConnected = true;
+      if (this.isConnected(map)) {
+        connected = true;
       }
     }
 
     //Replace the null values with spaces
     for (let row = 0; row < RAWS.settings.rows; row++) {
       for (let col = 0; col < RAWS.settings.cols; col++) {
-        if (level[row][col] === null) {
-          level[row][col] = CONSTS.SPACE;
+        if (map[row][col] === null) {
+          map[row][col] = CONSTS.SPACE;
         }
       }
     }
-    return level;
+    return map;
   },
 
-  generateBorder: function (level) {
-    for (let col = 0; col < level[0].length; col++) {
-      level[0][col], level[level.length-1][col]  = RAWS.map.text.wall;
+  generateBorder: function (map) {
+    for (let col = 0; col < map[0].length; col++) {
+      map[0][col], map[map.length-1][col]  = RAWS.map.text.wall;
     }
 
-    for (let row = 0; row < level.length; row++) {
-      level[row][0], level[row][level[row].length] = RAWS.map.text.wall;
+    for (let row = 0; row < map.length; row++) {
+      map[row][0], map[row][map[row].length] = RAWS.map.text.wall;
     }
 
-    return level;
+    return map;
   },
 
   generateRoom: function () {
@@ -240,7 +246,7 @@ let LEVEL = {
     return room;
   },
 
-  isConnected: function (level) {
+  isConnected: function (map) {
     //TODO: Optimize for larger maps
     return true;
   }
@@ -257,7 +263,7 @@ let connected = initializeMatrix(RAWS.settings.rows, RAWS.settings.cols, false);
         x = getRandomCoordinate(RAWS.settings.rows);
         y = getRandomCoordinate(RAWS.settings.cols);
 
-        if (level[x][y] === RAWS.map.text.floor) {
+        if (map[x][y] === RAWS.map.text.floor) {
             isFloor = true;
         }
     }
@@ -276,7 +282,7 @@ let connected = initializeMatrix(RAWS.settings.rows, RAWS.settings.cols, false);
         for (let row = (checkX-1); row < checkX+2; row++) {
             for (let col = (checkY-1); col < checkY+2; col++){
                 if (row !== checkX || col !== checkY) {
-                    if (level[row][col] === RAWS.map.text.floor && !connected[row][col]) {
+                    if (map[row][col] === RAWS.map.text.floor && !connected[row][col]) {
                         connected[row][col] = true;
                         floorList.push([row,col]);
                     }
@@ -287,7 +293,7 @@ let connected = initializeMatrix(RAWS.settings.rows, RAWS.settings.cols, false);
 
     for (let row = 0; row < RAWS.settings.rows; row++) {
         for (let col = 0; col < RAWS.settings.cols; col++) {
-            if (level[row][col] === RAWS.map.text.floor && connected[row][col] === false) {
+            if (map[row][col] === RAWS.map.text.floor && connected[row][col] === false) {
                 return false;
             }
         }
